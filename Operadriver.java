ced3c1587b5f687e3450718ed60ad403a65d01e0
sirisha_D <45494190+sirishadeshu@users.noreply.github.com>
2021-07-08 22:53:06 +0530

    Automation files added to git

--- Operadriver.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.firefox.*;
-import org.openqa.selenium.*;
-public abstract class Operadriver {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
-}
